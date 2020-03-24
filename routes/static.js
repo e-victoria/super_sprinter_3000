9583b50b15a376b1a79f3137ce020e47f8d45b42
e-victoria <evictoria@protonmail.com>
2020-03-24 22:37:19 +0100

    Initialize project

---
+++ routes/static.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+const port = 3000
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use('/', express.static('public'));
+
+module.exports = app;
